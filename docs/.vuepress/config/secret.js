66710a85aca65c5e47e8bbbd7c4f08e893cb9682
wzm-htdz <huangtdz@gmail.com>
2022-10-09 21:38:09 +0800

    删除了私密信息

--- docs/.vuepress/config/secret.js
+++ docs/.vuepress/config/secret.js
@@ -1,9 +1,9 @@
 module.exports = {
-  clientId: 'c3541062f9a7e2a8d7fd',
-  clientSecret: '26915d150dff588418a9a8661623ef88df821c76',
-  'ga': 'UA-243964559-1',
+  clientId: '',
+  clientSecret: '',
+  'ga': '',
   keyPage: {
-    keys: ['3884ed360dc8053d3adcf80e3748beb0'], // 1.3.0 版本后需要设置为密文
+    keys: [''], // 1.3.0 版本后需要设置为密文
     color: '#42b983', // 登录页动画球的颜色
     lineColor: '#42b983' // 登录页动画线的颜色
   }
